35d2d38c60634e0024fc3c0a301f9fd2cdeaca8c
stLmpp <gui.stlmpp@gmail.com>
2024-08-13 23:35:27 -0300

    - change egg random effect chances

--- src/main/java/com/stlmpp/spigot/plugins/events/EggRandomEffectEvent.java
+++ src/main/java/com/stlmpp/spigot/plugins/events/EggRandomEffectEvent.java
@@ -24,30 +24,35 @@ public class EggRandomEffectEvent implements Listener {
 
   private EggRandomEffectEvent(StlmppPlugin plugin) {
     this.plugin = plugin;
-    this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
-    this.chance = this.plugin.config.getDouble(StlmppPluginConfig.eggRandomEventChance);
-    this.plugin.log(
-        String.format("Egg Random Effect activated with %s chance of happening", this.chance));
-    this.entitiesToSpawn.add(1.0, EntityType.WARDEN);
-    this.entitiesToSpawn.add(2.0, EntityType.WITHER);
-    this.entitiesToSpawn.add(4.0, EntityType.RAVAGER);
-    this.entitiesToSpawn.add(8.0, EntityType.SHULKER);
-    this.entitiesToSpawn.add(8.0, EntityType.EVOKER);
-    this.entitiesToSpawn.add(16.0, EntityType.BLAZE);
-    this.entitiesToSpawn.add(16.0, EntityType.VEX);
-    this.entitiesToSpawn.add(16.0, EntityType.VINDICATOR);
-    this.entitiesToSpawn.add(16.0, EntityType.GHAST);
-    this.entitiesToSpawn.add(32.0, EntityType.WITHER_SKELETON);
-    this.entitiesToSpawn.add(32.0, EntityType.PILLAGER);
+    plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    chance = plugin.config.getDouble(StlmppPluginConfig.eggRandomEventChance);
+    plugin.log(String.format("Egg Random Effect activated with %s chance of happening", chance));
+    entitiesToSpawn
+        .add(1.0, EntityType.WARDEN)
+        .add(2.0, EntityType.WITHER)
+        .add(4.0, EntityType.RAVAGER)
+        .add(4.0, EntityType.SILVERFISH)
+        .add(4.0, EntityType.ENDERMITE)
+        .add(6.0, EntityType.EVOKER)
+        .add(10.0, EntityType.SHULKER)
+        .add(16.0, EntityType.BLAZE)
+        .add(16.0, EntityType.VINDICATOR)
+        .add(16.0, EntityType.GHAST)
+        .add(20.0, EntityType.SPIDER)
+        .add(36.0, EntityType.WITHER_SKELETON)
+        .add(36.0, EntityType.PILLAGER);
 
-    this.quantitiesToSpawn.add(1.0, 8);
-    this.quantitiesToSpawn.add(2.0, 7);
-    this.quantitiesToSpawn.add(4.0, 6);
-    this.quantitiesToSpawn.add(16.0, 5);
-    this.quantitiesToSpawn.add(32.0, 4);
-    this.quantitiesToSpawn.add(32.0, 3);
-    this.quantitiesToSpawn.add(64.0, 2);
-    this.quantitiesToSpawn.add(64.0, 1);
+    quantitiesToSpawn
+        .add(0.25, 12)
+        .add(1.0, 10)
+        .add(2.0, 8)
+        .add(4.0, 7)
+        .add(8.0, 6)
+        .add(16.0, 5)
+        .add(28.0, 4)
+        .add(32.0, 3)
+        .add(48.0, 2)
+        .add(52.0, 1);
   }
 
   private final WeightedRandomCollection<EntityType> entitiesToSpawn =
@@ -65,12 +70,12 @@ public class EggRandomEffectEvent implements Listener {
     if (!(damager instanceof Egg)
         || !(entity instanceof LivingEntity livingEntity)
         || entity.getLocation().getY() < 62
-        || !Rng.chance(this.chance)) {
+        || !Rng.chance(chance)) {
       return;
     }
-    final var spawnQuantity = this.quantitiesToSpawn.next();
+    final var spawnQuantity = quantitiesToSpawn.next();
     for (int index = 0; index < spawnQuantity; index++) {
-      final var entityToSpawn = this.entitiesToSpawn.next();
+      final var entityToSpawn = entitiesToSpawn.next();
       final var randomLocation =
           Optional.of(livingEntity.getLocation())
               .map(
@@ -81,7 +86,6 @@ public class EggRandomEffectEvent implements Listener {
               .get();
       final var spawnLocation = randomLocation.clone();
       spawnLocation.add(0, 5, 0);
-      final var plugin = this.plugin;
       final var seconds = (index + 0.1) * index * 0.50;
       plugin.runLater(
           Tick.fromSeconds(seconds),
